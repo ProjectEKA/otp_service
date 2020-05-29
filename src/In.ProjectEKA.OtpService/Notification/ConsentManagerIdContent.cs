d3d13662e22fb7fbc654122f43025c7dd1b70f6b
Rajeswari Gottipati <rajeswag@thoughtwork.com>
2020-05-29 12:22:35 +0530

    |CM_APP_57|Added test and refactor the code

---
+++ src/In.ProjectEKA.OtpService/Notification/ConsentManagerIdContent.cs
@@ -0,0 +1,12 @@
+namespace In.ProjectEKA.OtpService.Notification
+{
+    public class ConsentManagerIdContent
+    {
+        public string ConsentManagerId { get; }
+
+        public ConsentManagerIdContent(string consentManagerId)
+        {
+            ConsentManagerId = consentManagerId;
+        }
+    }
+}
