2fd30be11fa88afd8dd8c629ce5c40c0f1318d1b
MD Dubey <mddubey409@gmail.com>
2020-11-13 12:53:11 +0530

    Dubey | testing numbers variable; needs to be reverted

--- src/In.ProjectEKA.OtpService/Otp/OtpSenderFactory.cs
+++ src/In.ProjectEKA.OtpService/Otp/OtpSenderFactory.cs
@@ -2,6 +2,8 @@ namespace In.ProjectEKA.OtpService.Otp
 {
 	using System.Collections.Generic;
 	using System.Linq;
+    using Common.Logger;
+    using System;
 
 	public class OtpSenderFactory
     {
@@ -20,8 +22,16 @@ namespace In.ProjectEKA.OtpService.Otp
 
         public IOtpSender ServiceFor(string mobileNumber)
         {
-            if (mobileNumber != null && whitelistedNumbers.Any(number => number.Contains(mobileNumber)))
+            Console.WriteLine("Checking for mobile number");
+            Console.WriteLine(mobileNumber);
+            Console.WriteLine("All numbers");
+            foreach (string s in whitelistedNumbers)
             {
+                Console.WriteLine(s);
+            }
+            if (mobileNumber != null && whitelistedNumbers.Any(number => number.Contains(mobileNumber)))            
+            {
+                Console.WriteLine("matched");
                 return fakeOtpSender;
             }
 
